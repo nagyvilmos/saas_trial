8391e27cb35cc08f3a36f2414bd24a58bec05a54
William Norman-Walker <nagy_vilmos@hotmil.com>
2016-11-10 10:10:18 +0000

    Amend new profile handler

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,7 @@ class ProfilesController < ApplicationController
     def new
         # form for each user to fill out there own profile
         @user = User.find( params[:user_id] )
-        @profile = @user.build_profile
+        @profile = Profile.new
     end
     
     def create
