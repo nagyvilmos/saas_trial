a2216293a3eeaac5cc0d45a93cefbe5a4100934a
William Norman-Walker <nagy_vilmos@hotmil.com>
2016-11-10 15:31:51 +0000

    Add index page listing all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
     before_action :authenticate_user!
     
     def index
+        @users = User.all #what happens when this get's bigly?
     end
     
     def show
