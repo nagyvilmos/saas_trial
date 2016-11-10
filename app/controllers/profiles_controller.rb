38f0a6d9975213984060678c8d270685789832ad
William Norman-Walker <nagy_vilmos@hotmil.com>
2016-11-10 12:08:56 +0000

    Restrict profile and user to logged in and current user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,18 +1,20 @@
 class ProfilesController < ApplicationController
-
+    before_action :authenticate_user!
+    before_action :only_current_user
+    
     def new
         # form for each user to fill out there own profile
-        @user = User.find( params[:user_id] )
+        #@user = User.find( params[:user_id] )
         @profile = Profile.new
     end
     
     def create
         # create a user profile
-        @user = User.find( params[:user_id] )
+        #@user = User.find( params[:user_id] )
         @profile = @user.build_profile(profile_params)
         if @profile.save
             flash[:success] = "Profile updated"
-            redirect_to user_path( params[:user_id] )
+            redirect_to user_path( @user.id )
         else
             render action: :new 
         end
@@ -20,12 +22,28 @@ class ProfilesController < ApplicationController
     
     def edit
         # edit an existing profile.
-        @user = User.find( params[:user_id] )
+        #@user = User.find( params[:user_id] )
         @profile = @user.profile
     end
     
+    def update
+        # create a user profile
+        #@user = User.find( params[:user_id] )
+        @profile = @user.profile # then?
+        if @profile.update_attributes(profile_params)
+            flash[:success] = "Profile updated"
+            redirect_to user_path( @user.id )
+        else
+            render action: :edit 
+        end
+    end
     private
         def profile_params
             params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+        end
+        
+        def only_current_user
+            @user = User.find( params[:user_id] )
+            redirect_to(root_url) unless @user == current_user
         end
 end
